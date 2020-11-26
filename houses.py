7b9bdd5b0a87515959bc2b27a10b50c575e9824c
vihini1208 <74893959+vihini1208@users.noreply.github.com>
2020-11-26 04:36:55 -0700

    Add files via upload

---
+++ houses.py
@@ -0,0 +1,11 @@
+x=int(input("enter the number; "))
+if x%5==0:
+    print("sapu")
+elif x%5==1:
+    print("na")
+elif x%5==2:
+    print("olu")
+elif x%5==3:
+    print("nelum")
+else:
+    print("pichcha")
